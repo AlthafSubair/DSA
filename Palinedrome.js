869535c18dd52fd7e6c9371b1f21ab2f42368aac
Althaf Subair <althafsubair3@gmail.com>
2025-06-14 18:32:32 +0530

    completed reverse integer problem

--- Palinedrome.js
+++ Palinedrome.js
@@ -1,11 +1,11 @@
 
 function Palinedrome(str){
     const pal = str.split("").reverse().join("");
-    if(pal === str){
+    if(pal == str){
         return true
     }else{
         return false
     }
 }
 
-console.log(Palinedrome("malayalam"))
+console.log(Palinedrome(151))
---
+++ ReverseInt.js
@@ -0,0 +1,21 @@
+
+
+function ReverseInt(num){
+
+    let res = 0;
+
+    while(num != 0){
+
+        let rem = num % 10;
+
+        res = (res * 10) + rem;
+
+
+        num = Math.floor(num / 10);
+
+    }
+
+    return res
+}
+
+console.log(ReverseInt(145));
